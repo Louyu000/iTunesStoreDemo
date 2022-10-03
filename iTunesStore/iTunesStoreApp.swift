09588ddd0442de44f00e59cddd6971d46b699ca8
Louyu000 <louyu50310@gmail.com>
2022-10-03 20:02:37 +0800

    Initial Commit

---
+++ iTunesStore/iTunesStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesStoreApp.swift
+//  iTunesStore
+//
+//  Created by User17 on 2022/10/3.
+//
+
+import SwiftUI
+
+@main
+struct iTunesStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
